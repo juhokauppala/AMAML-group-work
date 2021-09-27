647f5e02d9c06a043d296518a2cee22e11bec45c
JReeder091 <91432381+JReeder091@users.noreply.github.com>
2021-09-27 22:04:12 +0300

    Update Pretreat_JK.m
    Added additional code to the removal of downtime section to clarify the downtime time and make plots. Function added at end to assist.

--- Week 3/Pretreat_JK.m
+++ Week 3/Pretreat_JK.m
@@ -5,10 +5,36 @@ addpath("../dataset");
 all_data = get_data();
 varnames = all_data.varnames;
 time_full = all_data.time;
+time_full = time_full - min(time_full) + 1;
 nvars = length(varnames);
 
 % Normalize
 normalized = normalize(all_data.values);
+
+%% Remove downtime period
+% Given how much data is available, removing everything before downtime
+% period could help prevent different performance from before maintainance
+% affecting the result on a maintained system.
+
+% Plot before
+data_raw = all_data.values;
+x = 21;
+subtimeplots(time_full, data_raw, x:min(x+4-1, nvars), varnames);
+
+% Based on histogram(time_full), the maintenance seems to be between
+% 7.36772e5 and 7.36782e5
+% When reset to 0, time threshold becomes 20 = min(time_full) + 19
+TIME_THRESHOLD = min(time_full) + 19;
+after_maintenance_filter = time_full > TIME_THRESHOLD;
+after_maintenance_time = time_full(after_maintenance_filter);
+after_maintenance_data = data_raw(after_maintenance_filter, :);
+
+% To apply:
+time_full = after_maintenance_time;
+data_raw = after_maintenance_data;
+
+% Plot after
+subtimeplots(time_full, data_raw, x:min(x+4-1, nvars), varnames);
 
 %% Remove Iron Outlet
 % Variables got renamed in get_data() from the cleaner ones with spaces 
@@ -17,22 +43,6 @@ idx = find(contains(varnames,'IronConcentrate'));
 normalized(:,idx) = [];
 varnames(idx) = [];
 nvars = nvars - length(idx);
-
-%% Remove downtime period
-% Given how much data is available, removing everything before downtime
-% period could help prevent different performance from before maintainance
-% affecting the result on a maintained system.
-
-% Based on histogram(time_full), the maintenance seems to be between
-% 7.36772e5 and 7.36782e5
-TIME_THRESHOLD = 7.3678e5;
-after_maintenance_filter = time_full > TIME_THRESHOLD;
-after_maintenance_time = time_full(after_maintenance_filter);
-after_maintenance_data = normalized(after_maintenance_filter, :);
-
-% To apply:
-% time_full = after_maintenance_time
-% normalized = after_maintenance_data
 
 %% Outlier Removal
 % PCA
@@ -71,3 +81,17 @@ sgtitle({"Histograms of T^2 values","(Percent Reduction in Data = " + ...
 % % variable only updates every hour, using data for every 20s is excessive.
 % % Still working out how to do this tho.
 
+%% functions
+
+function subtimeplots(time, data, columns, names)
+    figure
+    width = 1;
+    height = ceil(length(columns)/width);
+    j = 0;
+    for i = columns
+        j = j + 1;
+        subplot(height, width, j)
+        scatter(time, data(:,i), '.')
+        title(convertCharsToStrings(names{i}))
+    end
+end
