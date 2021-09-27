0029149ba0e40d2d882b5b4db46d5d7fcfa76822
Juho Kauppala <35465459+juhokauppala@users.noreply.github.com>
2021-09-27 14:16:57 +0300

    Outliers are removed

--- Week 3/Pretreat_JK.m
+++ Week 3/Pretreat_JK.m
@@ -7,3 +7,12 @@ data = get_data();
 
 % Normalize
 normalized = normalize(data.values);
+
+% Pca
+[COEFF, SCORE, LATENT, TSQUARED, EXPLAINED] = pca(normalized);
+
+% Outliers
+OUTLIER_THRESHOLD = 100;
+outliers = TSQUARED >= OUTLIER_THRESHOLD;
+data_without_outliers = normalized(~outliers, :);
+
