e7678286bf5418df46d22a53ef6b923bd4cd70bf
JReeder091 <91432381+JReeder091@users.noreply.github.com>
2021-10-15 15:51:06 +0300

    Update pcr.m
    Fixed incorrect assignment of number of PCs. Added a title to the prediction plot.

--- Week 5/pcr.m
+++ Week 5/pcr.m
@@ -1,10 +1,10 @@
-function [result] = pcr(data)
+function rsqPCR = pcr(data)
 %PCR Summary of this function goes here
 %   Detailed explanation goes here
 %% Prep data
 X = data(:,1:end-1);
 Y = data(:,end);
-[n m] = size(data);
+[n, ~] = size(data);
 Yc = Y - mean(Y);
 
 %% Get Principle Components (PCA)
@@ -15,30 +15,29 @@ Yc = Y - mean(Y);
 % plot(1:10, 100*cumsum(PCAVar(1:10))/sum(PCAVar(1:10)));
 % xlabel('Num of PC')
 % ylabel('Explained Variance in X');
-pc = find(100*cumsum(PCAVar(1:10))>90,1,'first');
+pc = find(100*cumsum(PCAVar)/sum(PCAVar)>=90,1,'first');
 
-%% NEW CONTENT - Principle Component Regression
-% Step 2: Get regression factors for each of the 4 PCs
+%% Principle Component Regression
+% Get regression factors for each of the 4 PCs
 betaPCR = regress(Yc, PCAScores(:,1:pc));
 
-% Step 3: Transform B's from PCs to Beta Coefficients for actual vars
+% Transform B's from PCs to Beta Coefficients for actual vars
 betaPCR = PCALoadings(:,1:pc)*betaPCR;
 betaPCR = [mean(Y) - mean(X)*betaPCR; betaPCR];
 
-% Step 4: Generate prediction
+% Generate prediction
 yfitPCR = [ones(n,1) X]*betaPCR;
 
 % Visually test fit of prediction
 figure;
-plot(Yc, yfitPCR, 'bo');
+plot(Y, yfitPCR, 'bo');
 xlabel('Observed Response')
 ylabel('Fitted Response')
+title("PCR (PC: " + num2str(pc) + ")");
 
 % Find statistical r^2
 TSS = sum(Yc.^2);
 RSS = sum((Y - yfitPCR).^2);
 rsqPCR = 1 - RSS/TSS
 
-%% Compute result
-result = [];
 end
