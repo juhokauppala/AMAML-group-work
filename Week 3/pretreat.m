64107139c1e4be8f31ec0f1e876e1da058d26f98
JReeder091 <91432381+JReeder091@users.noreply.github.com>
2021-10-16 15:49:59 +0300

    Update pretreat.m
    Had to rewrite some parts of plotting functions bc I made the inputs too confusing. Also moved the normalization of the dataset to the end to make it easier to read it at various steps (still normalized for parts that need it, just not saved into the variable). Added removal of time samples whenever data samples are removed to keep them in sync. Also removed the shift of the output variable back to the sample before it. It makes performance worse, so I assume they must have already adjusted it. Commented out some of the histograms bc I didn't find them useful at this stage.

--- Week 3/pretreat.m
+++ Week 3/pretreat.m
@@ -5,9 +5,6 @@ time_full = raw_data.time;
 time_full = time_full - min(time_full) + 1;
 nvars = length(varnames);
 
-% Normalize
-data_norm = normalize(raw_data.values);
-
 %% Remove downtime period
 % Given how much data is available, removing everything before downtime
 % period could help prevent different performance from before maintainance
@@ -15,8 +12,7 @@ data_norm = normalize(raw_data.values);
 
 % Plot before
 data_raw = raw_data.values;
-x = 21;
-subtimeplots(time_full, data_raw, x:min(x+4-1, nvars), varnames);
+subtimeplots(time_full, data_raw(:,[1:4 end]), varnames([1:4 end]));
 
 % Based on histogram(time_full), the maintenance seems to be between
 % 7.36772e5 and 7.36782e5
@@ -32,58 +28,69 @@ time_full = after_maintenance_time;
 data_raw = after_maintenance_data;
 
 % Plot after
-subtimeplots(time_full, data_raw, x:min(x+4-1, nvars), varnames);
+subtimeplots(time_full, data_raw(:,[1:4 end]), varnames([1:4 end]));
 
 %% Remove Iron Outlet
 % Variables got renamed in get_data() from the cleaner ones with spaces 
 % we had before, we might want them back by the end
 idx = find(contains(varnames,'IronConcentrate'));
-data_norm(:,idx) = [];
+data_raw(:,idx) = [];
 varnames(idx) = [];
-nvars = nvars - length(idx);
+nvars = nvars - 1;
 
 %% Outlier Removal
 % PCA
-[~, ~, ~, TSQUARED, ~] = pca(data_norm);
+[~, ~, ~, TSQUARED, ~] = pca(normalize(data_raw));
 
 % Outliers
 OUTLIER_THRESHOLD = 100;
 outliers = TSQUARED >= OUTLIER_THRESHOLD;
-data_outr = data_norm(~outliers, :);
-p_reduced = 100*(1 - size(data_outr,1)/size(data_norm,1));
+data_outr = data_raw(~outliers, :);
+time_full = time_full(~outliers, :);
+p_reduced = 100*(1 - size(data_outr,1)/size(data_raw,1));
+fprintf('Dataset reduced by %2.2f%% with outlier removal\n',p_reduced);
+
 
 % Plot results with histograms
-figure
-nbins = 200;
-subplot(2,1,1)
-histogram(TSQUARED,nbins)
-subplot(2,1,2)
-histogram(TSQUARED(~outliers),nbins)
-sgtitle({"Histograms of T^2 values","(Percent Reduction in Data = " + ...
-                                        num2str(round(p_reduced,1)) + "%)"})
+% figure
+% nbins = 200;
+% subplot(2,1,1)
+% histogram(TSQUARED,nbins)
+% subplot(2,1,2)
+% histogram(TSQUARED(~outliers),nbins)
+% sgtitle({"Histograms of T^2 values","(Percent Reduction in Data = " + ...
+%                                         num2str(round(p_reduced,1)) + "%)"})
 
-                                    
 %% Average fast (20s inc) variables into 1hr inc, using 1hr inc time var
 time = unique(time_full);
 n = length(time);
 treated = zeros(length(time),size(data_outr,2));
-for t = 1:length(time)
+for t = 1:n
     idxa = find(time_full == time(t),1,'first');
     idxb = find(time_full == time(t),1,'last');
     treated(t,:) = mean(data_outr(idxa:idxb,:));
 end
+% CANCELLED - DOING THIS WORSENS RESULTS, DATA MUST ALREADY BE ADJUSTED.
 % Align output silica conc to the previous process values that actually
 % determine it:
-for t = 1:length(time)-1
-    treated(t,end) = treated(t+1,end);
-end
-treated = treated(1:end-1,:); % Remove incomplete final sample
+% lag = 1;    % hrs lag time before pred var affected
+% for t = 1:n-lag
+%     treated(t,end) = treated(t+lag,end);
+% end
+% treated = treated(1:end-lag,:); % Remove incomplete final sample(s)
+% time = time(1:end-lag,:);       % Remove incomplete final sample(s)
+
+%% Normalize cleaned data
+treated = normalize(treated);
 
 %% Plot cleaned/treated dataset
 subhists(treated, 3:7, varnames);
 subhists(treated, 8:14, varnames);
 subhists(treated, 15:nvars-1, varnames);
 subhists(treated, [1 2 nvars], varnames);
+
+% Time plot after
+subtimeplots(time, treated(:,[1:4 end]), varnames([1:4 end]));
 
 %% functions
 
@@ -101,15 +108,15 @@ function subhists(data, columns, names)
     end
 end
 
-function subtimeplots(time, data, columns, names)
+function subtimeplots(time, data, names)
     figure
     width = 1;
-    height = ceil(length(columns)/width);
+    height = ceil(size(data,2)/width);
     j = 0;
-    for i = columns
+    for i = 1:height*width
         j = j + 1;
         subplot(height, width, j)
-        scatter(time, data(:,i), '.')
+        plot(time, data(:,i))
         title(convertCharsToStrings(names{i}))
     end
 end
