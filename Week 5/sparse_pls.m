a05ccd21ae4b2577f63c9e0920bab6bcfe2b3bfc
unknown <juho.kauppala@gmail.com>
2021-10-17 22:17:42 +0300

    SPLS - testing with internet code

--- Week 5/sparse_pls.m
+++ Week 5/sparse_pls.m
@@ -10,6 +10,31 @@ function [result] = sparse_pls(data)
 X = data(:,1:end-1);
 Y = data(:,end);
 
+%[u, v, success] = my_spls(X, Y, sqrt(21), 1);
+[u, v, success] = spls(X, Y, sqrt(21), 1);
+if ~success
+    warning('SPLS not successful');
+end
+
+estimates = sum(X .* u', 2);
+disp("SPLS stats:");
+total_var = sum((Y - mean(Y)).^2)
+residuals = sum((estimates - Y).^2)
+r2 = 1 - (residuals / total_var)
+result = [];
+end
+
+function [b] = S(a, lambda)
+b = sign(a) * max((abs(a) - lambda), 0);
+end
+
+% UNFINISHED
+function [U_out, V_out] = iterate(V, C)
+U_new = C * V;
+end
+
+% UNFINISHED - DO NOT USE YET
+function [u, v, success] = my_spls(X, Y, su, sv)
 % 1 Let C <- X'Y
 C = X' * Y;
 
@@ -23,12 +48,9 @@ if (norm(V, 2) ~= 1)
 end
 
 % 3 Repeat until convergence
-
-
-
-result = [];
+is_ready = false;
+while ~is_ready
+    [u, v] = iterate(V, C);
 end
-
-function [b] = S(a, lambda)
-b = sign(a) * max((abs(a) - lambda), 0);
+success = false;
 end
