2300260471eec13a6c431832c2277608185c4170
JReeder091 <91432381+JReeder091@users.noreply.github.com>
2021-10-18 21:36:29 +0300

    Update kernel_pls.m
    Add kernel_pls function

--- Week 5/kernel_pls.m
+++ Week 5/kernel_pls.m
@@ -1,6 +1,43 @@
-function [result] = kernel_pls(data)
+function rsqKPLS = kernel_pls(data)
 %PLS Summary of this function goes here
 %   Detailed explanation goes here
-result = [];
-end
+%% Prep data
+% Get data
+X = data(:,1:end-1);
+Y = data(:,end);
 
+% Meta variables
+m = size(X,2);
+
+% Normalize
+X = zscore(X);
+
+%% Finding PCs
+[~, ~, ~, ~, ~, PCTVAR, ~, ~] = ...
+                                    plsregress(X, Y, m);
+pc = find(cumsum(100*PCTVAR(2,:))/sum(PCTVAR(2,:))>95,1);
+
+%% Compute Kernel Matrix
+kerneltype = 'gaussian'; mod = "gamma"; modval = 10;    % Seems to improve with higher modval, likely overfitting
+% kerneltype = 'sigmoid'; mod = "gamma"; modval = 0.1;  % 16%
+% kerneltype = 'laplacian'; mod = "gamma"; modval = 0.02; % 22%
+% kerneltype = 'polynomial'; mod = "degree"; modval = 4;
+
+kfun = Kernel('type', kerneltype, mod, modval);
+kmat = kfun.computeMatrix(X, X);
+
+%% Compute K-PLS
+[~, Yest, ~] = dualpls(kmat,kmat,Y,pc);
+
+%% Get Results
+TSS = sum(Y.^2);
+RSS = sum((Y-Yest).^2);
+rsqKPLS = 1 - RSS/TSS;
+fprintf('Rsq for K-PLS = %2.2f%%\n',100*rsqKPLS);
+
+%% Visually test fit of prediction
+figure;
+plot(Y, Yest, 'b.');
+xlabel('Observed Response')
+ylabel('Estimated Response')
+title("K-PLS (PC: " + num2str(pc) + ")");
